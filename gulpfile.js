c2427b64198cf9cde25c04d00765687c2451a280
Sina Kuhestani <sinakuhestani@gmail.com>
2025-03-29 02:36:08 +0330

    complete foundation + test files

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,86 @@
 'use strict';
 
 import gulp from 'gulp';
-import sass from 'gulp-sass';
-import sassCompiler from 'sass';
+import * as sass from 'sass';
+import gulpSass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
 import concat from 'gulp-concat';
 import autoprefixer from 'gulp-autoprefixer';
+import cleanCSS from 'gulp-clean-css';
+import pug from 'gulp-pug';
+import htmlbeautify from 'gulp-html-beautify';
+import header from 'gulp-header';
+import ts from 'gulp-typescript';
+import uglify from 'gulp-uglify';
+import browserSync from 'browser-sync';
+import plumber from 'gulp-plumber';
 
-const sassC = sass(sassCompiler);
+const sassC = gulpSass(sass);
+const tsProject = ts.createProject('tsconfig.json');
+
+// Banner comment for JS
+const jsBanner = `/*!
+ * My JavaScript Library v1.0.0
+ * Author: Your Name
+ * License: MIT
+ * Description: A brief description of the JavaScript library.
+ */\n`;
+
+// BrowserSync instance
+const server = browserSync.create();
 
 function buildStyles() {
     return gulp.src('scss/**/*.scss')
-        .pipe(sourcemaps.init()) // Initialize sourcemaps
-        .pipe(sassC({ outputStyle: 'compressed' }).on('error', sassC.logError)) // Compile SCSS to CSS
+        .pipe(plumber()) // Prevent pipe breaking caused by errors
+        .pipe(sourcemaps.init())
+        .pipe(sassC({ outputStyle: 'compressed' }).on('error', sassC.logError))
         .pipe(autoprefixer({ 
-            overrideBrowserslist: ['last 2 versions', '> 1%', 'ie >= 10'], // Adjust browser support as needed
+            overrideBrowserslist: ['last 2000 versions', '> 0.1%', 'ie >= 8'],
             cascade: false 
-        })) // Add prefixes
-        .pipe(concat('all.css')) // Concatenate all CSS files
-        .pipe(sourcemaps.write('.')) // Write sourcemaps
-        .pipe(gulp.dest('./dist')); // Output to the dist folder
+        }))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(header(jsBanner))
+        .pipe(concat('all.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(server.stream()); // Inject changes without reloading
 }
 
-function watch() {
-    gulp.watch('scss/**/*.scss', buildStyles); // Watch for changes in SCSS files
+function compilePug() {
+    return gulp.src('pug/**/*.pug')
+        .pipe(plumber())
+        .pipe(pug())
+        .pipe(htmlbeautify({
+            indentSize: 4,
+            unformatted: ['a', 'span'],
+            indent: '    ',
+            maxPreserveNewlines: 1,
+        }))
+        .pipe(gulp.dest('./dist'))
+        .pipe(server.stream());
 }
 
-export { buildStyles, watch };
+function compileTypeScript() {
+    return tsProject.src()
+        .pipe(plumber())
+        .pipe(tsProject())
+        .pipe(header(jsBanner))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(server.stream());
+}
+
+function serve() {
+    server.init({
+        server: {
+            baseDir: './dist'
+        }
+    });
+
+    gulp.watch('scss/**/*.scss', buildStyles);
+    gulp.watch('pug/**/*.pug', compilePug);
+    gulp.watch('ts/**/*.ts', compileTypeScript);
+    gulp.watch('./dist/*.html').on('change', server.reload);
+}
+
+export { buildStyles, compilePug, compileTypeScript, serve };
